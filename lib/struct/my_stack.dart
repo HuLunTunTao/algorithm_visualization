e62c806bd829de20c65bb7c5a10e269e5dfdbb1c
HuLunTunTao <3094289517@qq.com>
2025-09-04 16:10:56 +0800

    实现了栈

---
+++ lib/struct/my_stack.dart
@@ -0,0 +1,22 @@
+import 'my_singly_linked_list.dart';
+
+class MyStack<T> {
+  late MySinglyLinkedList<T> list;
+  MyStack(){
+    list=MySinglyLinkedList<T>();
+  }
+  
+  void push(T data)=>list.insertHead(data);
+
+  void pop()=>list.removeTail();
+
+  T? top()=>list.getTail();
+
+  int get length=>list.length;
+
+  bool isEmpty()=>list.isEmpty();
+
+  @override
+  String toString() => list.toString();
+
+}
---
+++ lib/struct/test/my_stack_test.dart
@@ -0,0 +1,58 @@
+import 'dart:math';
+
+import 'package:flutter_test/flutter_test.dart';
+
+import '../my_stack.dart';
+
+void main(){
+  final random=Random();
+
+  final maxRandomInt=100;
+
+  MyStack<int> stack=MyStack<int>();
+
+  int numToInsert=random.nextInt(maxRandomInt);
+
+  print("入栈单个元素 $numToInsert");
+  stack.push(numToInsert);
+  print(stack);
+
+  print("出栈单个元素");
+  stack.pop();
+  print(stack);
+
+  print("入栈15个元素");
+  for(int i=0;i<15;i++){
+    stack.push(random.nextInt(maxRandomInt));
+  }
+  print(stack);
+
+  print("获取栈顶元素");
+  print(stack);
+  print(stack.top());
+  stack.pop();
+
+  print(stack);
+  print(stack.top());
+  stack.pop();
+
+  print(stack);
+  print(stack.top());
+  stack.pop();
+
+  test("元素测试", (){
+    print("当前元素数是否为12");
+    print("当前元素数 ${stack.length}");
+    expect(stack.length,12);
+
+    print("出栈直到空");
+    while(!stack.isEmpty()){
+      stack.pop();
+    }
+    expect(stack.isEmpty(),true);
+    expect(stack.length, 0);
+  });
+
+
+
+}
