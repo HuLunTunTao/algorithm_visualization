712c7bdd1f7903ed358476e6669fbc4d5133e015
HuLunTunTao <3094289517@qq.com>
2025-09-04 15:35:19 +0800

    完善了单向链表对单个插入和删除的测试

--- lib/struct/test/my_singly_linked_list_test.dart
+++ lib/struct/test/my_singly_linked_list_test.dart
@@ -5,21 +5,22 @@ import '../my_singly_linked_list.dart';
 void main(){
   print("测试插入");
   var random = Random();
+  final maxRandomInt=100;
 
   MySinglyLinkedList<int> list=MySinglyLinkedList<int>();
   for(int i=0;i<10;i++){
-    list.insertHead(random.nextInt(100));
+    list.insertHead(random.nextInt(maxRandomInt));
   }
   list.printAll();
   print("链表长度：${list.length}");
 
-  int numToInsert=random.nextInt(100);
+  int numToInsert=random.nextInt(maxRandomInt);
   print("在头部插入数字$numToInsert");
   list.insertHead(numToInsert);
   list.printAll();
   print("链表长度：${list.length}");
 
-  numToInsert=random.nextInt(100);
+  numToInsert=random.nextInt(maxRandomInt);
   print("在尾部插入数字$numToInsert");
   list.insertTail(numToInsert);
   list.printAll();
@@ -44,6 +45,28 @@ void main(){
   list.removeTail();
   list.printAll();
   print("链表长度：${list.length}");
+
+  print("全部清空");
+  list.clear();
+  list.printAll();
+  print("链表长度：${list.length}");
+
+  print("头部插入一个 从尾部删除一个");
+  list.insertHead(random.nextInt(maxRandomInt));
+  list.printAll();
+  print("链表长度：${list.length}");
+  list.removeTail();
+  list.printAll();
+  print("链表长度：${list.length}");
+
+  print("尾部插入一个 从头部删除一个");
+  list.insertTail(random.nextInt(maxRandomInt));
+  list.printAll();
+  print("链表长度：${list.length}");
+  list.removeHead();
+  list.printAll();
+  print("链表长度：${list.length}");
+
 
 
 }
