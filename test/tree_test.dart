3fb4c7d8372d8f94a0f1376cc36feac60b344615
程翊雪 <1355784761@qq.com>
2025-09-05 16:49:29 +0800

    重构了一下树的代码，使其能在节点类中接纳知识点类

--- test/tree_test.dart
+++ test/tree_test.dart
@@ -1,110 +1,76 @@
+// test_tree.dart
+
 import 'dart:io';
-import '../lib/struct/tree.dart';
+import '../lib/algo/tree_kmp.dart'; // 导入你的 KMP 算法文件
+import '../lib/struct/tree.dart'; // 导入你的树结构代码文件
 
-// main 函数现在被设计为异步，以便使用 await 暂停程序等待用户输入
-void main() async {
-  // 1. 创建一个 MyTree 实例
-  // 这里的 <dynamic> 类型让你可以存储不同类型的值
-  final myTree = MyTree<dynamic>('数据结构与算法', '编程基础知识');
+// 主测试函数
+void main() {
+  print('--- 开始测试 ---');
 
-  // 2. 调用 buildTree 方法，自动构建树结构
-  myTree.buildTree();
-  final rootNode = myTree.root;
+  // 创建一个根节点
+  final rootKnowledge = Knowledge('root_topic', 0, 0.0);
+  final tree = MyTree<Knowledge>(rootKnowledge);
+  final rootNode = tree.root;
 
-  // -------------------------------------------------------------
-  // 3. 基础功能测试：DFS 遍历和层级获取
-  // -------------------------------------------------------------
-  print('--- 步骤三：基础功能测试 ---');
-  print('开始进行深度优先遍历打印：');
-  myTree.dfsPrint(rootNode);
+  // 1. 测试添加节点
+  print('\n## 1. 测试添加节点');
+  final nodeA = tree.addNode(Knowledge('Math_Fundamentals', 10, 0.6), rootNode);
+  final nodeB = tree.addNode(Knowledge('Physics_Basics', 15, 0.7), rootNode);
+  final nodeA1 = tree.addNode(Knowledge('Calculus_I', 20, 0.8), nodeA);
+  final nodeA2 = tree.addNode(Knowledge('Linear_Algebra', 25, 0.9), nodeA);
 
-  final firstLevelNode = rootNode.children[0];
-  final secondLevelNode = firstLevelNode.children[0];
+  // 2. 测试 DFS 打印树结构
+  print('\n## 2. 测试 DFS 打印树结构');
+  tree.dfsPrint(rootNode);
 
-  print('\n节点层级测试：');
-  print('${rootNode.name} 的层级是: ${myTree.getNodeLevel(rootNode)}');
-  print('${firstLevelNode.name} 的层级是: ${myTree.getNodeLevel(firstLevelNode)}');
-  print('${secondLevelNode.name} 的层级是: ${myTree.getNodeLevel(secondLevelNode)}');
-  print('-------------------------------------------------------------');
-  
-  // -------------------------------------------------------------
-  // 4. DFS 查找测试：查找用户输入的节点
-  // -------------------------------------------------------------
-  print('--- 步骤四：DFS 查找测试 ---');
-  print('请输入你要查找的节点名称（例如：cc223d）：');
-  String? sname = stdin.readLineSync();
+  // 3. 测试获取节点层级
+  print('\n## 3. 测试获取节点层级');
+  print('根节点 "${rootNode.value.name}" 的层级是: ${tree.getNodeLevel(rootNode)}');
+  print('节点 "${nodeA.value.name}" 的层级是: ${tree.getNodeLevel(nodeA)}');
+  print('节点 "${nodeA1.value.name}" 的层级是: ${tree.getNodeLevel(nodeA1)}');
 
-  if (sname != null && sname.isNotEmpty) {
-    // 你的 dfsFind 方法返回 int，这会导致类型转换错误，但为了保持你的代码不变，
-    // 我们在此处直接调用，并捕捉可能的错误。
-    // 在实际项目中，应修改 dfsFind 的返回值类型为 T?。
-    try {
-      final result = myTree.dfsFind(rootNode, sname);
-      if (result != -1) {
-        print('找到节点，其值为: $result');
-      } else {
-        print('未找到节点');
-      }
-    } catch (e) {
-      print('查找出错：${e.toString()}');
-      print('提示：这是因为 dfsFind 期望节点值为 int，但你的 buildTree 设置为 String。');
-    }
+  // 4. 测试 DFS 搜索 (使用 kmp 模糊匹配)
+  print('\n## 4. 测试 DFS 搜索');
+  final foundNode = tree.dfsFind(rootNode, 'Calculus');
+  if (foundNode != null) {
+    print('通过模糊搜索 "Calculus" 找到节点: "${foundNode.value.name}"');
+  } else {
+    print('未找到包含 "Calculus" 的节点。');
   }
-  print('-------------------------------------------------------------');
 
-  // -------------------------------------------------------------
-  // 5. 修改节点名称和值的功能测试
-  // -------------------------------------------------------------
-  print('--- 步骤五：修改名称和值测试 ---');
-  final nodeToUpdate = rootNode.children[0]; // 选择 'ababa' 节点
+  final notFoundNode = tree.dfsFind(rootNode, 'Chemistry');
+  if (notFoundNode != null) {
+    print('找到节点: ${notFoundNode.value.name}');
+  } else {
+    print('未找到包含 "Chemistry" 的节点。');
+  }
 
-  print('修改前，节点信息:');
-  print('名称: ${nodeToUpdate.name}, 值: ${nodeToUpdate.value}');
+  // 5. 测试修改节点名称
+  print('\n## 5. 测试修改节点名称和学习时间');
+  tree.updateName(nodeA2, 'Discrete_Mathematics',123);
+  print('修改后，再次打印树结构：');
+  tree.dfsPrint(rootNode);
 
-  myTree.updateName(nodeToUpdate, "修改后的ababa", 9999);
-
-  print('\n修改后，节点信息:');
-  print('名称: ${nodeToUpdate.name}, 值: ${nodeToUpdate.value}');
-  print('-------------------------------------------------------------');
+  // 6. 测试删除节点 (保留子节点)
+  print('\n## 6. 测试删除节点 (保留子节点)');
+  final nodeC = tree.addNode(Knowledge('Quantum_Mechanics', 30, 0.95), nodeB);
+  final nodeC1 = tree.addNode(Knowledge('Wave_Function', 35, 0.85), nodeC);
+  print('删除前，树结构为:');
+  tree.dfsPrint(rootNode);
   
-  // -------------------------------------------------------------
-  // 6. 删除功能测试
-  // -------------------------------------------------------------
-  print('--- 步骤六：删除功能测试 ---');
+  tree.deleteNode_withson(nodeC);
+  print('删除 "Quantum_Mechanics" 后，树结构为:');
+  tree.dfsPrint(rootNode);
 
-  // 6a. 测试保留子节点的删除 (deleteNode_withson)
-  print('\n-- 测试保留子节点的删除 --');
-  // 创建一个新树实例以进行测试，避免影响之前的测试结果
-  final myTree_withson = MyTree<dynamic>('根节点', '初始值');
-  myTree_withson.addNode('A', 1, myTree_withson.root);
-  myTree_withson.addNode('B', 2, myTree_withson.root.children[0]);
-  myTree_withson.addNode('C', 3, myTree_withson.root.children[0]);
+  // 7. 测试删除节点 (不保留子节点)
+  print('\n## 7. 测试删除节点 (不保留子节点)');
+  print('删除前，树结构为:');
+  tree.dfsPrint(rootNode);
   
-  print('删除前，树结构：');
-  myTree_withson.dfsPrint(myTree_withson.root);
-  
-  final nodeToDelete_withson = myTree_withson.root.children[0]; // 节点A
-  myTree_withson.deleteNode_withson(nodeToDelete_withson);
+  tree.deleteNode_withoutson(nodeB);
+  print('删除 "Physics_Basics" 后，树结构为:');
+  tree.dfsPrint(rootNode);
 
-  print('\n删除节点 "${nodeToDelete_withson.name}" 后，树结构：');
-  myTree_withson.dfsPrint(myTree_withson.root);
-  print('-------------------------------------------------------------');
-
-  // 6b. 测试连带子节点的删除 (deleteNode_withoutson)
-  print('\n-- 测试连带子节点的删除 --');
-  // 创建一个新树实例以进行测试
-  final myTree_withoutson = MyTree<dynamic>('根节点', '初始值');
-  myTree_withoutson.addNode('D', 4, myTree_withoutson.root);
-  myTree_withoutson.addNode('E', 5, myTree_withoutson.root.children[0]);
-  myTree_withoutson.addNode('F', 6, myTree_withoutson.root.children[0]);
-  
-  print('删除前，树结构：');
-  myTree_withoutson.dfsPrint(myTree_withoutson.root);
-
-  final nodeToDelete_withoutson = myTree_withoutson.root.children[0]; // 节点D
-  myTree_withoutson.deleteNode_withoutson(nodeToDelete_withoutson);
-
-  print('\n删除节点 "${nodeToDelete_withoutson.name}" 后，树结构：');
-  myTree_withoutson.dfsPrint(myTree_withoutson.root);
-  print('-------------------------------------------------------------');
+  print('\n--- 测试结束 ---');
 }
